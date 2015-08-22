23d9d2c643e144ff57f7b5ec84200827628c435b
ksurdyk <lincoln2491@gmail.com>
2015-08-22 19:20:17 +0200

    remove angles features

--- run_analysis.R
+++ run_analysis.R
@@ -2,15 +2,14 @@ if(!file.exists("getdata_projectfiles_UCI HAR Dataset.zip")){
   download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", 
                 destfile = "getdata_projectfiles_UCI HAR Dataset.zip", method = "curl")
 }
-if(!file.exists("household_power_consumption.txt")){
+if(!file.exists("UCI HAR Dataset")){
   unzip("getdata_projectfiles_UCI HAR Dataset.zip")
 }
 
 activities = read.table("UCI HAR Dataset/activity_labels.txt")
 colnames(activities) = c("id", "activity_label")
 features = read.table("UCI HAR Dataset/features.txt")
-colToUse = features$V2[grepl("Mean", features$V2) | 
-                         grepl("mean", features$V2) | grepl("std", features$V2) ]
+colToUse =  as.character(features$V2[grepl("mean", features$V2) | grepl("std", features$V2) ])
 
 
 testDataX = read.table("UCI HAR Dataset/test/X_test.txt")
@@ -20,6 +19,7 @@ colnames(testDataX) = features$V2
 colnames(testDataY) = "labels"
 colnames(testDataSubject) = "subject"
 testDataX = testDataX[ colToUse]
+colnames(testDataX) = gsub("\\(\\)", "", colnames(testDataX))
 
 trainDataX = read.table("UCI HAR Dataset/train/X_train.txt")
 trainDataY = read.table("UCI HAR Dataset/train/y_train.txt")
@@ -28,6 +28,7 @@ colnames(trainDataX) = features$V2
 colnames(trainDataY) = "labels"
 colnames(trainDataSubject) = "subject"
 trainDataX = trainDataX[ colToUse]
+colnames(trainDataX) = gsub("\\(\\)", "", colnames(trainDataX))
 
 testDF = data.frame(testDataSubject, testDataY, testDataX)
 trainDF = data.frame(trainDataSubject, trainDataY, trainDataX)
@@ -37,3 +38,5 @@ df$labels =  with(activities,  activity_label[match(df$labels, id)])
 
 
 ag = aggregate(df[, -2:-1], list(df$subject, df$labels), mean)
+
+write.table(ag, row.names = FALSE, file = "tidy_data.txt")
